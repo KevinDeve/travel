191337c1a0ffc9863dd00a90a8ee0a5d6cf51a6a
kevin <166177784+KevinDeve@users.noreply.github.com>
2024-05-16 18:31:35 -0500

    Add files via upload

---
+++ proyectos/script.js
@@ -0,0 +1,7 @@
+const menuBtn=document.querySelector(".menu-btn")
+const navLink=document.querySelector(".nav-links")
+
+menuBtn.addEventListener("click", ()=> {
+    navLink.classList.toggle("mobile-menu")
+
+})
